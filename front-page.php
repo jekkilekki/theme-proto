083a347879c509390584d4fa641b5b3e1d36eabb
Aaron Snowberger <jekkilekki@gmail.com>
2015-05-06 12:40:40 +0900

    Add Clients list to front page

--- front-page.php
+++ front-page.php
@@ -3,6 +3,8 @@
 /**
  * The custom template for the one-page style front page. Kicks in automatically
  * @link: http://www.designwall.com/guide/dw-page/ (Good for design ideas)
+ * 
+ * @TODO: Add CONDITIONAL statements to test for the existence of each of the pages - OR add dummy content to tell people to fill those in
  */
 
 get_header(); ?>
@@ -13,41 +15,36 @@ global $more;
 
 	<div id="primary" class="content-area front-page<?php if (!(have_comments() || comments_open())) : ?> no-comments-area<?php endif; ?>">
 		<div id="content" class="site-content" role="main">
-                    
-                     <!--<section id="home">
-                        <div class="indent clear">-->
 
-                            <?php
-                            $query = new WP_Query( 'pagename=home' );
+                    <?php
+                    $query = new WP_Query( 'pagename=home' );
 
-                            // The Loop
-                            if ( $query->have_posts() ) {
-                                while ( $query->have_posts() ) {
-                                    $query->the_post();
-                                    
-                                    $url_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "full" );
-                                    if ( !empty( $url_image ) ) {
-                                        echo "<section id='home' style='background:url($url_image[0]) repeat fixed; background-size: cover;'>";
-                                    } else {
-                                        echo "<section id='home'>";
-                                    }
-                                    echo '<div class="indent clear">';
-                                    
-                                    echo '<div class="entry-content">';
-                                    the_content();
-                                    echo '</div>';
-                                    
-                                    echo '</div><!-- .indent -->';
-                                    echo '</section><!-- #home -->';
-                                    
-                                }
+                    // The Loop
+                    if ( $query->have_posts() ) {
+                        while ( $query->have_posts() ) {
+                            $query->the_post();
+
+                            $url_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "full" );
+                            if ( !empty( $url_image ) ) {
+                                echo "<section id='home' style='background:url($url_image[0]) repeat fixed; background-size: cover;'>";
+                            } else {
+                                echo "<section id='home'>";
                             }
-                            // Restore original Post Data
-                            wp_reset_postdata();
-                            ?>
+                            echo '<div class="indent clear">';
 
-                        <!--</div><!-- .indent -->
-                    <!--</section><!-- #home -->
+                            echo '<div class="entry-content">';
+                            the_content();
+                            echo '</div>';
+
+                            echo '</div><!-- .indent -->';
+                            echo '</section><!-- #home -->';
+
+                        }
+                    }
+                    // Restore original Post Data
+                    wp_reset_postdata();
+                    ?>
+                    
                     
                     <section id="services">
                         <div class="indent clear">
@@ -180,6 +177,52 @@ global $more;
                                     echo '</div>';
                                 }
                                 echo '</div>';
+                            }
+                            // Restore original Post Data
+                            wp_reset_postdata();
+                            ?>
+                            
+                            <?php
+                            $query = new WP_Query( 'pagename=clients' );
+                            $clients_id = $query->queried_object->ID;
+                            
+                            // The Loop
+                            if ( $query->have_posts() ) {
+                                while ( $query->have_posts() ) {
+                                    $query->the_post();
+                                    $more = 0;      // Set (inside the loop) to display content above the more tag
+                                    echo '<h2 class="entry-header">' . get_the_title() . '</h2>';
+                                    echo '<div class="entry-content">';
+                                    the_content('');
+                                    echo '</div>';
+                                }
+                            }
+                            // Restore original Post Data
+                            wp_reset_postdata();
+                            
+                            // Get the children of the clients page
+                            $args = array(
+                                'post_type'     => 'page',
+                                'post_parent'   => $clients_id,
+                                'posts_per_page'=> 6,
+                                'order_by'      => 'rand'
+                            );
+                            $clients_query = new WP_Query( $args );
+                            
+                            // The Loop
+                            if ( $clients_query->have_posts() ) {
+                                echo '<ul class="clients-list">';
+                                while ( $clients_query->have_posts() ) {
+                                    $clients_query->the_post();
+                                    $more = 0;
+                                    echo '<li class="clear">';
+                                    echo '<a class="clients-link" href="' . get_permalink() . '" title="See all Projects for ' . get_the_title() . '">';
+                                    echo '<h3 class="services-title">' . get_the_title() . '</h3>';
+                                    the_post_thumbnail( 'portfolio' );
+                                    echo '</a>';
+                                    echo '</li>';
+                                }
+                                echo '</ul>';
                             }
                             // Restore original Post Data
                             wp_reset_postdata();
